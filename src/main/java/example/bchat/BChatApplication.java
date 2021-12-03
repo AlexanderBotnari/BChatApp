f6728baea7a8cb9db8b2802c2222a9ad4bcda552
unknown <sandubotnari99@gmail.com>
2021-12-03 11:10:55 +0200

    init'

---
+++ src/main/java/example/bchat/BChatApplication.java
@@ -0,0 +1,12 @@
+package example.bchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BChatApplication.class, args);
+	}
+}
